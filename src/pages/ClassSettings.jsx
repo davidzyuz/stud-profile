23ff81130da576005230b76a076d4e42b8c62993
David Zyuz <zyuzdavid@gmail.com>
2024-04-17 14:47:28 +0300

    basic class settings layout

--- src/pages/ClassSettings.jsx
+++ src/pages/ClassSettings.jsx
@@ -1,7 +1,20 @@
+import { Grid, GridItem, Box } from "@chakra-ui/react";
+
 export default function ClassSettings() {
   return (
-    <div>
-      <h1>Class Settings</h1>
-    </div>
+    <Grid templateColumns="repeat(6, 1fr)" gap={6} bg="grey.50" m="10px">
+      <GridItem colSpan={6} bg="tomato">
+        <Box>
+          <h1>Class Settings</h1>
+          <span>some filter</span>
+        </Box>
+      </GridItem>
+      <GridItem colSpan={2} bg="lightgreen" minHeight="100vh">
+        <h1>Class Settings</h1>
+      </GridItem>
+      <GridItem colSpan={4} bg="lightblue" minHeight="100vh">
+        <h1>Specific class</h1>
+      </GridItem>
+    </Grid>
   );
 }
