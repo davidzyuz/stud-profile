91cef5526a9919e055048c0b943cd5e0bdff18ab
David Zyuz <zyuzdavid@gmail.com>
2024-04-22 18:27:03 +0300

    removed debug styles

--- src/components/AddNewStudent.jsx
+++ src/components/AddNewStudent.jsx
@@ -18,13 +18,12 @@ export default function AddNewStudent({ onAddNewStudent }) {
       h="100vh"
       top="0"
       left="0"
-      border="1px solid red"
       backgroundColor="rgba(0, 0, 0, 0.8)"
       backdropFilter="blur(10px)"
       zIndex={1000}
     >
       <GridItem colSpan={2} onClick={onAddNewStudent} />
-      <GridItem colSpan={1} backgroundColor="white" border="1px solid green">
+      <GridItem colSpan={1} backgroundColor="white">
         <VStack>
           <Text as="h1">Add new student</Text>
           <Text>An email will be send</Text>
